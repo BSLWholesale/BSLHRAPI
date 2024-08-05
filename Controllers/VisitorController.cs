6c3577ba872f753e4e3ba518e6382086d7e61240
Kanchan <banswaraws@gmail.com>
2024-08-05 17:51:05 +0530

    2nd Git Upload

--- Controllers/VisitorController.cs
+++ Controllers/VisitorController.cs
@@ -15,22 +15,22 @@ namespace BSLHRAPI.Controllers
 
         DALVisitor _DALVisitor = new DALVisitor();
 
-        [System.Web.Http.HttpPost]
-        [System.Web.Http.Route("api/Visitor/Fn_Insert_Visitor")]
-        public clsVisitor Fn_Insert_Visitor(clsVisitor objReq)
-        {
-            var objResp = new clsVisitor();
-            objResp = _DALVisitor.Fn_Insert_Visitor(objReq);
-            return objResp;
-        }
+        //[System.Web.Http.HttpPost]
+        //[System.Web.Http.Route("api/Visitor/Fn_Insert_Visitor")]
+        //public clsVisitor Fn_Insert_Visitor(clsVisitor objReq)
+        //{
+        //    var objResp = new clsVisitor();
+        //    objResp = _DALVisitor.Fn_Insert_Visitor(objReq);
+        //    return objResp;
+        //}
 
-        [System.Web.Http.HttpPost]
-        [System.Web.Http.Route("api/Visitor/Fn_Get_All_Visitor")]
-        public clsVisitor Fn_Get_All_Visitor(clsVisitor objReq)
-        {
-            var objResp = new clsVisitor();
-            objResp = _DALVisitor.Fn_Get_All_Visitor(objReq);
-            return objResp;
-        }
+        //[System.Web.Http.HttpPost]
+        //[System.Web.Http.Route("api/Visitor/Fn_Get_All_Visitor")]
+        //public clsVisitor Fn_Get_All_Visitor(clsVisitor objReq)
+        //{
+        //    var objResp = new clsVisitor();
+        //    objResp = _DALVisitor.Fn_Get_All_Visitor(objReq);
+        //    return objResp;
+        //}
     }
 }
